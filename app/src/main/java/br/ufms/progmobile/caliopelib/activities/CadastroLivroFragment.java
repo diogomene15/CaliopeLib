0f655076ed60df54f74f07eeb094f746d7476ba7
diogomene <diogodelimamenezes15@gmail.com>
2024-06-26 20:40:13 -0400

    feat: adds take picture feature inside LivroCadastro

--- app/src/main/java/br/ufms/progmobile/caliopelib/activities/CadastroLivroFragment.java
+++ app/src/main/java/br/ufms/progmobile/caliopelib/activities/CadastroLivroFragment.java
@@ -1,14 +1,26 @@
 package br.ufms.progmobile.caliopelib.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
+
 import br.ufms.progmobile.caliopelib.R;
+import br.ufms.progmobile.caliopelib.databinding.FragmentCadastroLivroBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,51 +28,82 @@ import br.ufms.progmobile.caliopelib.R;
  * create an instance of this fragment.
  */
 public class CadastroLivroFragment extends Fragment {
+    private static final int CAMERA_PERMISSION_CODE = 1;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentCadastroLivroBinding binding;
+    private ActivityResultLauncher<Uri> tirarFotoLaunchner;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    Uri imageUri;
     public CadastroLivroFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CadastroLivroFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CadastroLivroFragment newInstance(String param1, String param2) {
+    private Uri createUri(){
+        String fileName = "image" + System.currentTimeMillis() + ".jpg";
+        File imagemLivro = new File(getContext().getFilesDir(), fileName);
+        return FileProvider.getUriForFile(
+                getContext(),
+                "br.ufms.progmobile.caliopelib.fileprovider",
+                imagemLivro
+        );
+    }
+
+    private void registerPictureLauncher(){
+        tirarFotoLaunchner = registerForActivityResult(
+                new ActivityResultContracts.TakePicture(),
+                new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        try {
+                            if(result){
+                                System.out.println("Sucesso "+ imageUri.toString());
+                                binding.livroImageView.setImageURI(null);
+                                binding.livroImageView.setImageURI(imageUri);
+                            }
+                        }catch (Exception e){
+                            e.getStackTrace();
+                        }
+                    }
+                }
+        );
+    }
+
+    private void checkAndOpenCamera(){
+        if(ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.CAMERA)
+        != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }else{
+            tirarFotoLaunchner.launch(imageUri);
+        }
+    }
+
+
+
+    public static CadastroLivroFragment newInstance() {
         CadastroLivroFragment fragment = new CadastroLivroFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cadastro_livro, container, false);
+
+        binding = FragmentCadastroLivroBinding.inflate(inflater, container, false);
+
+        imageUri = createUri();
+        registerPictureLauncher();
+
+        binding.addFotoButton.setOnClickListener(v -> {
+            checkAndOpenCamera();
+        });
+        return binding.getRoot();
     }
 }
