9b00364d0ed6f8e3815ae267ce6b5a4f3b18aa9a
diogomene <diogodelimamenezes15@gmail.com>
2024-06-26 21:27:37 -0400

    feat: Adds take picture feat into LivroDetalhe frag

--- app/src/main/java/br/ufms/progmobile/caliopelib/activities/LivroDetalheFragment.java
+++ app/src/main/java/br/ufms/progmobile/caliopelib/activities/LivroDetalheFragment.java
@@ -1,14 +1,27 @@
 package br.ufms.progmobile.caliopelib.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
+
 import br.ufms.progmobile.caliopelib.R;
+import br.ufms.progmobile.caliopelib.databinding.FragmentLivroDetalheBinding;
+import br.ufms.progmobile.caliopelib.entities.Livro;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,51 +29,114 @@ import br.ufms.progmobile.caliopelib.R;
  * create an instance of this fragment.
  */
 public class LivroDetalheFragment extends Fragment {
+    private static final int CAMERA_PERMISSION_CODE = 1;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentLivroDetalheBinding binding;
+    private ActivityResultLauncher<Uri> tirarFotoLaunchner;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    Uri imageUri;
     public LivroDetalheFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LivroDetalheFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LivroDetalheFragment newInstance(String param1, String param2) {
+    private Uri createUri(){
+        String fileName = "image" + System.currentTimeMillis() + ".jpg";
+        File imagemLivro = new File(getContext().getFilesDir(), fileName);
+        return FileProvider.getUriForFile(
+                getContext(),
+                "br.ufms.progmobile.caliopelib.fileprovider",
+                imagemLivro
+        );
+    }
+
+    private void registerPictureLauncher(){
+        tirarFotoLaunchner = registerForActivityResult(
+                new ActivityResultContracts.TakePicture(),
+                new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        try {
+                            if(result){
+                                System.out.println("Sucesso "+ imageUri.toString());
+                                binding.newLivroImageView.setImageURI(null);
+                                binding.newLivroImageView.setImageURI(imageUri);
+                            }
+                        }catch (Exception e){
+                            e.getStackTrace();
+                        }
+                    }
+                }
+        );
+    }
+
+    private void checkAndOpenCamera(){
+        if(ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }else{
+            tirarFotoLaunchner.launch(imageUri);
+        }
+    }
+
+
+
+    public static LivroDetalheFragment newInstance(Livro livro) {
         LivroDetalheFragment fragment = new LivroDetalheFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putSerializable("livro", livro);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_livro_detalhe, container, false);
+
+        binding = FragmentLivroDetalheBinding.inflate(inflater, container, false);
+
+        imageUri = createUri();
+        registerPictureLauncher();
+
+        if (getArguments() != null) {
+
+            Livro livroParam = (Livro) getArguments().getSerializable("livro");
+            if(livroParam != null){
+
+                String tituloLivro = livroParam.getTitulo();
+                if(tituloLivro!=null){
+                    binding.newTituloEditText.setText(tituloLivro);
+                }
+
+                String descricaoLivro = livroParam.getDescricao();
+                if(descricaoLivro != null){
+                    binding.newDescricaoEditText.setText(descricaoLivro);
+                }
+
+                float avaliacaoLivro = livroParam.getAvaliacao();
+                if(avaliacaoLivro > 0){
+                    binding.newRatingBar.setRating(avaliacaoLivro);
+                }
+
+                String imageUriString = livroParam.getFotoPath();
+
+                if (imageUriString != null) {
+                    Uri livroUri = Uri.parse(imageUriString);
+                    binding.newLivroImageView.setImageURI(null);
+                    binding.newLivroImageView.setImageURI(livroUri);
+                }
+            }
+
+        }
+
+        binding.newAddFotoButton.setOnClickListener(v -> {
+            checkAndOpenCamera();
+        });
+        return binding.getRoot();
     }
 }
